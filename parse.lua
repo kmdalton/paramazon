5324eb85d72184fae488236fa1c17ffd292bda43
James Koppen <jk@jameskoppen.com>
2013-10-17 15:38:10 -0500

    Basic parser prototype.

---
+++ parse.lua
@@ -0,0 +1,40 @@
+require "lxp"
+require "io"
+
+body = io.input("test.xml"):read("*a")
+ 
+-- parseTree = lxp.lom.parse(body)
+
+function In(str, tbl)
+  for _, v in pairs(tbl) do
+    if str == v then return true end
+  end
+end
+
+elements = {"Author", "Title", "Creator", "DetailPageURL"}
+
+callbacks = {
+  StartElement = function(parser, name)
+    if In(name, elements) then
+      io.write(name, ": ")
+      callbacks.CharacterData = function(parser, string)
+        io.write(string, "\n")
+      end
+    end
+  end,
+
+  EndElement = function(parser, name)
+    if In(name, elements) then
+      callbacks.CharacterData = false
+    elseif name == "ItemAttributes" then
+      io.write("---\n")
+    end
+  end,
+
+  CharacterData = false
+}
+
+parser = lxp.new(callbacks)
+
+parser:parse(body)
+parser:close()
