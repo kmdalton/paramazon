fb0fce565273230da661cf8444fed1040ca04639
James Koppen <jk@jameskoppen.com>
2013-10-18 00:07:40 -0500

    Folded socket.http:request() into affiliate_request lib.

--- test.lua
+++ test.lua
@@ -1,15 +1,11 @@
 require "affiliate_request"
-http = require("socket.http")
 
 r = requester:new("private.conf")
-r["Operation"]="ItemSearch"
 r["Keywords"]="Lua"
 r["ItemPage"]="1"
 r["SearchIndex"]="Books"
 
-url = r:request()
-
-b, c, h = http.request(url)
+b, c, h = r:request(url)
 
 body = io.open("test.xml", "w+")
 body:write(b)
