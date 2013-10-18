9bc5a7c9a05d1b51e1675289f6eb21dac94bf383
James Koppen <jk@jameskoppen.com>
2013-10-18 11:09:55 -0500

    CLI app to evolve into web app.

---
+++ app.lua
@@ -0,0 +1,19 @@
+require "affiliate_request"
+local lom = require "lxp.lom"
+local cjson = require("cjson")
+
+args = {}
+for i,v in pairs(arg) do
+  if i > 0 then args[i] = v end
+end
+
+keywords = table.concat(args, " ")
+
+r = requester:new("private.conf")
+r["Keywords"] = keywords
+r["ItemPage"] = "1"
+r["SearchIndex"] = "Books"
+
+body = r:request()
+
+print(cjson.encode(lom.parse(body)))
