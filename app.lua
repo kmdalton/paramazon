910e842f30f13016771ee88fcdf625a66fcd7bc6
James Koppen <jk@jameskoppen.com>
2013-10-18 18:09:20 -0500

    First pass at a Heroku ready app.lua and nginx.conf. Need to add an index.html and figure out AWS config handling. May need to use Heroku ENV vars.

--- app.lua
+++ app.lua
@@ -1,20 +1,56 @@
 require "affiliate_request"
-local lom = require "lxp.lom"
-local cjson = require("cjson")
+require "lxp"
+local json = require("cjson")
 
-args = {}
-for i,v in pairs(arg) do
-  if i > 0 then args[i] = v end
+if not ngx.var.arg_keywords then
+  ngx.say("Provide some keywords")
 end
 
-keywords = table.concat(args, " ")
+local args = ngx.req.get_uri_args()
 
-r = requester:new("private.conf")
-r["Keywords"] = keywords
+local r = requester:new("private.conf")
+r["Keywords"] = args.keywords
 r["ItemPage"] = "1"
-r["SearchIndex"] = "Books"
-r["ResponseGroup"]="ItemAttributes, Reviews"
+r["SearchIndex"] = "Books" -- "Blended" for all product types
+r["ResponseGroup"] = "ItemAttributes, Reviews"
 
-body = r:request()
+local body = r:request()
 
-print(cjson.encode(lom.parse(body)))
+function In(str, tbl)
+  for _, v in pairs(tbl) do
+    if str == v then return true end
+  end
+end
+
+local elements = {
+  "Author", "Title", "Creator", "DetailPageURL", "FormattedPrice", "IFrameURL", "TotalReviews"
+}
+local parsed = {}
+local item = {}
+
+callbacks = {
+  StartElement = function(parser, name)
+    if In(name, elements) then
+      callbacks.CharacterData = function(parser, string)
+        item[name] = string
+      end
+    end
+  end,
+
+  EndElement = function(parser, name)
+    if In(name, elements) then
+      callbacks.CharacterData = false
+    elseif name == "Item" then
+      table.insert(parsed, item); item = {}
+    end
+  end,
+
+  CharacterData = false
+}
+
+local parser = lxp.new(callbacks)
+
+parser:parse(body)
+parser:close()
+
+ngx.say(json.encode(parsed))
