77aaa81fd0905c75ed2143acc28b36bec829527a
Kevin Dalton <kmdalton@stanford.edu>
2015-06-17 23:47:50 -0700

    minor restructuring
    	modified:   app.py
    	modified:   config.txt

--- app.py
+++ app.py
@@ -8,23 +8,29 @@
 
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application, url, asynchronous
-import redis
+import redis, re
+
+#Go ahead, add more, I dare you.
+mandatory_params = [
+    'redis-host',
+    'redis-port',
+    'redis-db',
+    'AWS_ACCESS_KEY_ID',
+    'AWS_SECRET_ACCESS_KEY',
+    'AWS_ASSOCIATE_TAG',
+]
+
+def sanitize(string):
+    """
+    santize(str) returns space separated list of lowercase alphanumerics
+    """
+    words = string.split()
+    words = [re.sub(r'[^0-9A-Za-z]', '', i).lower() for i in words]
+    return ' '.join(sorted(words))
 
 class MainHandler(RequestHandler):
-    def initialize(self, **kw):
-        pass
     def get(self):
-        pass
-    def post(self):
-        pass
-
-class AmazonSearchHandler(RequestHandler):
-    def initialize(self, **kw):
-        pass
-    def get(self):
-        pass
-    def post(self):
-        pass
+        self.render('templates/index.html')
 
 class SearchResultsHandler(RequestHandler):
     def initialize(self, **kw):
@@ -35,11 +41,26 @@ class SearchResultsHandler(RequestHandler):
         pass
 
 def main(config_filename):
-    kwargs = {}
+    kw = {}
     for line in open(config_filename):
         if line[0] != '#' and len(line.split()) == 2:
             k,v = line.split()
-            kwargs[k] = v
+            kw[k] = v
+
+    #Here we check to make sure all the mandatory params are present and quit if they are not
+    missing_params = [i for i in mandatory_params if i not in kw]
+    if len(missing_params) > 0:
+        print "Fatal error: missing the following parameters from {}".format(config_filename)
+        for i in missing_params:
+            print i 
+        sys.exit()
+
+    else:
+        application = Application([
+               (r"/", MainHandler, kw),
+               (r"/results", AmazonSearchHandler, kw),
+        ])
+
 
 if __name__=='__main__':
     config_filename = 'config.txt' #TODO: write a proper parser with flags
